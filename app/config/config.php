7eaac4056175bd017271509ffdae6acd7efd29e0
ElvanoRB <s2200259@student.unklab.ac.id>
2024-03-26 04:22:04 +0000

    commit pertama saya

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// define path constants
+define("APP_PATH", "http://103.250.10.171/unkpresent/public");
+
+// define database config
+define("HOST_DB", "localhost");
+define("NAME_DB", "db_attendance");
+define("USER_DB", "root");
+define("PASS_DB", "Abc2200259#");
